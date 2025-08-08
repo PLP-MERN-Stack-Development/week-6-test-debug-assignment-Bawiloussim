efb2031ef3e78ead37df01b029b68fde18e59b82
N'GBABOU Bawiloussim <bawiloussimngbabou1@gmail.com>
2025-08-08 11:33:21 +0000

    first update

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const server = require('./server');
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -8,5 +10,13 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+.then(() => {
+  console.log('MongoDB connected');
+  server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1); 
+});
